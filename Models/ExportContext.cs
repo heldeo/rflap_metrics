3eaeaaa8db5e89aca5cd68d2f63c2e20632f2487
heldeo <heldeo3@hotmail.com>
2020-05-17 15:05:38 -0500

    added connection strings and contexts to the requisite models

--- Models/ExportContext.cs
+++ Models/ExportContext.cs
@@ -4,6 +4,9 @@ namespace rflap_metrics.Models
 {
     public class ExportContext : DbContext
     {
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+}
         public ExportContext(DbContextOptions<ExportContext> options)
             : base(options)
         {
--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.EntityFrameworkCore;
+using rflap_metrics.Models;
 namespace rflap_metrics
 {
     public class Startup
@@ -25,6 +26,9 @@ namespace rflap_metrics
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ExportContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TestAndExportContext")));
+            services.AddDbContext<TestContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TestAndExportContext")));
+            services.AddDbContext<TestResultContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TestAndExportContext")));
             services.AddControllers();
         }
 
